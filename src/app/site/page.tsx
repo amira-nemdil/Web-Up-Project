1dcb700dfbca11837fc523bec119d4675e467e93
Lydiaa29 <lydiaarab5@gmail.com>
2024-11-23 23:41:51 +0100

    i modified the  image in page.tsx of site to clearly see the title web up

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -27,7 +27,7 @@ export default function Home(){
             webup
           </h1>
         </div>
-        <div className="flex justify-center items-center relative md:mt-[-70px]">
+        <div className="flex justify-center items-center relative md:mt-[-10px]">
           <Image
             src={'/assets/preview.png'}
             alt="banner image"
