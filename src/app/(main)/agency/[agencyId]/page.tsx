c556cf111111eb47a2a838f2b7533d722f51e356
Lydiaa29 <lydiaarab5@gmail.com>
2024-11-25 14:55:33 +0100

    add ;

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,5 +1,5 @@
-import CircleProgress from '@/components/global/circle-progress'
-import Link from 'next/link'
+import CircleProgress from '@/components/global/circle-progress';
+import Link from 'next/link';
 import React from "react";
 import { db } from "@/lib/db";
 import AgencyDetails from "@/components/forms/agency-details";
