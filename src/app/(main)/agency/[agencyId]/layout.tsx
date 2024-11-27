f3b5939fd6f59203e886db3cba837e608f6a0b6b
Lydiaa29 <lydiaarab5@gmail.com>
2024-11-27 17:37:11 +0100

    add layout.tsx to agencyId

---
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -0,0 +1,14 @@
+import { Chicle } from "next/font/google";
+import React from "react";
+
+type Props={
+    children: React.ReactNode 
+    params:{agencyId: string}
+} 
+
+const layout =({children, params }:Props)=>{
+
+    return <div>layout</div>
+}
+
+export default layout
