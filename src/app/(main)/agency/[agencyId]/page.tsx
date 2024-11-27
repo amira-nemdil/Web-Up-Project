849f4e2e7d04fb84fac2d824cee32ee9ec023889
Lydiaa29 <lydiaarab5@gmail.com>
2024-11-27 11:57:13 +0100

    decrale variables

--- src/app/(main)/agency/[agencyId]/page.tsx
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Page =({params }: {params :{agencyId:string}}) =>{
+  let currency ='USD'
+  let sessions
+  let totalClosedSessions
+  let totalPendingSessions
+  let net=0
+  let potentialIncome
+  let closingRate=0
+  
   return <div>{params.agencyId}</div>
 }
 
