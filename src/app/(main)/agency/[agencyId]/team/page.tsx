5760f6c01c35c619d45ec44917f932f394fc2256
Lydiaa29 <lydiaarab5@gmail.com>
2024-12-04 11:59:59 +0100

    add columns.tsx and fix page

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,7 +1,10 @@
 import { db } from '@/lib/db'
 import React from 'react'
+import DataTable from './data-table'
 import { Plus } from 'lucide-react'
 import { currentUser } from '@clerk/nextjs/server'
+import { columns } from './columns'
+import SendInvitation from '@/components/forms/send-invitation'
 
 type Props = {
   params: { agencyId: string }
@@ -34,10 +37,19 @@ const TeamPage = async ({ params }: Props) => {
   if (!agencyDetails) return
 
   return (
-  <DataTable>
-    
-  </DataTable>
-)
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+          Add
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue="name"
+      columns={columns}
+      data={teamMembers}
+    ></DataTable>
+  )
 }
 
 export default TeamPage
