07fb8995f6ea41a65e1011b18402ba5fa9054073
Lydiaa29 <lydiaarab5@gmail.com>
2024-12-05 14:33:03 +0100

    fix pipline

--- src/app/(main)/subaccount/[subaccountId]/piplines/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/piplines/page.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-type Props={}
-
-const Pipelines = (props :Props)=>{
-    return <div>Piplelines </div>
+type Props = {
+  params: { subaccountId: string }
 }
 
-export default Pipelines 
+const Pipelines = async ({ params }: Props) => {
+  const pipelineExists = await db.pipeline.findFirst({
+    where: { subAccountId: params.subaccountId },
+  })
+
+  if (pipelineExists)
+    return redirect(
+      `/subaccount/${params.subaccountId}/pipelines/${pipelineExists.id}`
+    )
+
+  try {
+    const response = await db.pipeline.create({
+      data: { name: 'First Pipeline', subAccountId: params.subaccountId },
+    })
+
+    return redirect(
+      `/subaccount/${params.subaccountId}/pipelines/${response.id}`
+    )
+  } catch (error) {
+    console.log()
+  }
+}
+
+export default Pipelines
