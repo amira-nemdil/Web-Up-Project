aba596a6b72c5b8c333dbdab37db93bbbc71bc46
Lydiaa29 <lydiaarab5@gmail.com>
2024-12-05 19:48:22 +0100

    add dropable to pipeline view

--- src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
@@ -145,7 +145,32 @@ const PipelineView = ({
             Create Lane
           </Button>
         </div>
-
+        <Droppable
+          droppableId="lanes"
+          type="lane"
+          direction="horizontal"
+          key="lanes"
+        >
+          {(provided) => (
+            <div
+              className="flex item-center gap-x-2 overflow-scroll"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+            </div>
+          )}
+        </Droppable>
+        {allLanes.length == 0 && (
+          <div className="flex items-center justify-center w-full flex-col">
+            <div className="opacity-100">
+              <Flag
+                width="100%"
+                height="100%"
+                className="text-muted-foreground"
+              />
+            </div>
+          </div>
+        )}
       </div>
     </DragDropContext>
   )
