5c68e2c7c9c665e10fd0cd338c9c2c4a7f6cd116
Lydiaa29 <lydiaarab5@gmail.com>
2024-12-08 13:41:07 +0100

    modify format to {format }

--- src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/contacts/page.tsx
@@ -11,8 +11,9 @@ import {
 } from '@/components/ui/table'
 import { db } from '@/lib/db'
 import { Contact, SubAccount, Ticket } from '@prisma/client'
+import {format} from 'date-fns/format'
 import React from 'react'
-
+import CraeteContactButton from './_components/create-contact-btn'
 
 type Props = {
   params: { subaccountId: string }
@@ -63,6 +64,7 @@ const ContactPage = async ({ params }: Props) => {
   return (
     <BlurPage>
       <h1 className="text-4xl p-4">Contacts</h1>
+      <CraeteContactButton subaccountId={params.subaccountId} />
       <Table>
         <TableHeader>
           <TableRow>
@@ -92,6 +94,7 @@ const ContactPage = async ({ params }: Props) => {
                   <Badge className="bg-emerald-700">Active</Badge>
                 )}
               </TableCell>
+              <TableCell>{format(contact.createdAt, 'MM/dd/yyyy')}</TableCell>
               <TableCell className="text-right">
                 {formatTotal(contact.Ticket)}
               </TableCell>
