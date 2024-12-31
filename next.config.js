ea782b365c594613a7c9e71041cffe22a666e333
Amira <amira.nemdil@hotmail.com>
2024-12-31 14:36:46 +0100

    Disable React Strict Mode to fix drag-and-drop issues

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'uploadthing.com',
-          'utfs.io',
-          'img.clerk.com',
-          'subdomain',
-          'files.stripe.com',
-        ],
-      },
+  reactStrictMode: false, // Disable React Strict Mode to fix drag-and-drop issues
+  images: {
+      domains: [
+        'uploadthing.com',
+        'utfs.io',
+        'img.clerk.com',
+        'subdomain',
+        'files.stripe.com',
+      ],
+  },
 };
 
 export default nextConfig;
